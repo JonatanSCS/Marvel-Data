e36fa06b4eef5a4a7015c264731e9039d551eed6
JonatanSCS <jonatan@inventivehack.com>
2017-04-26 09:19:34 -0500

    Add Comic List getter

--- src/App/scenes/Comics/containers/ComicList/ComicList.js
+++ src/App/scenes/Comics/containers/ComicList/ComicList.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
+import Comic from './components/Comic/Comic'
+
 class ComicListClass extends Component {
 
   constructor() {
     super()
 
+    this.state = {
+      comicList: []
+    }
+
     this.getComicsData = this.getComicsData.bind(this)
+    this.listNewComics = this.listNewComics.bind(this)
   }
 
   componentWillMount() {
@@ -17,22 +24,45 @@ class ComicListClass extends Component {
 
     const apiKey = '68b26e9204aa8011100128eb75e5b293'
     //const limitComics = 20;
-    const baseUrl = 'https://gateway.marvel.com/v1/public/comics?apikey=' + apiKey
+    const baseUrl = 'https://gateway.marvel.com/v1/public/comics?apikey=' + apiKey + '&orderBy=title'
 
     let requestUrl = baseUrl
 
     axios.get(requestUrl)
       .then((res) => {
-        console.log(res.data.data.results)
+        this.listNewComics(res.data.data.results)
       })
       .catch(function (err) {
         console.log(err)
       })
   }
 
+  listNewComics(comicsData) {
+    let comicsList = ''
+
+    if(comicsData.length !== 0) {
+       comicsList = comicsData.map((comic) =>
+        <Comic key={ comic.id } comicData={ comic }/>
+      )
+    } else {
+      comicsList = <h1>Sin resultados</h1>
+    }
+
+    if (this.refs.comicsList) {
+      this.setState({
+        comicList: comicsList
+      })
+    }
+
+  }
+
   render() {
     return(
-      <h1>Comic List Class</h1>
+      <div className="container">
+        <div ref="comicsList" className="row d-flex flex-wrap justify-content-around">
+          { this.state.comicList }
+        </div>
+      </div>
     )
   }
 }
